278be8e51cc22fd3dd8b28d382e39f0d2ac128ad
dennisNoguera98 <73449341+dennisNoguera98@users.noreply.github.com>
2020-11-22 22:00:31 -0400

    se agregaron archivos

---
+++ php/mostrar.php
@@ -0,0 +1,27 @@
+<?php
+
+	$inc=include ("conex.php");
+	if($inc){
+		$consulta = "SELECT * FROM resultados"
+		$resultado = mysqli_query($conex, $consulta);
+		if($resulado){
+			while($row = $resultado->fetch_array()){
+				$clientes_satisfechos = $row['clientes_satisfechos'];
+				$notebooks_reparados = $row['notebooks_reparados'];
+				$pc_escritorios_rep = $row['pc_escritorios_rep'];
+				$so_instalados = $row['so_instalados'];
+				?>
+
+					<div>
+						<h2><?php  php echo $clientes_satisfechos   ?></h2>
+						<h2><?php  php echo $notebooks_reparados   ?></h2>
+						<h2><?php  php echo $pc_escritorios_rep   ?></h2>
+						<h2><?php  php echo $so_instalados   ?></h2>
+					</div>
+
+				<?php
+			}
+		}
+	}
+
+?>
